3b9bcd0562262604cee300cef7e7f30bf258424b
Manan Shah <mshah151204@gmail.com>
2025-12-18 23:58:01 -0800

    assignment

--- assignment/assignment 2/q2.c
+++ assignment/assignment 2/q2.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    int value = 8;        // Binary: 00001000
-    int bitPosition = 3;  // We want to check the 4th bit (index 3)
-
-    // Move the bit we want to the far right, then check it with '& 1'
+    int value = 8;        
+    int bitPosition = 3;  
     if ((value >> bitPosition) & 1) {
         printf("Bit %d is set\n", bitPosition);
     } else {
