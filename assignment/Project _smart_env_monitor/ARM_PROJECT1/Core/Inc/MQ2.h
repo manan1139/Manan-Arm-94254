582e32da08319d6812e4d254a225b2d42ae87cef
Manan Shah <mshah151204@gmail.com>
2026-01-01 16:12:55 +0530

    assignment

---
+++ assignment/Project _smart_env_monitor/ARM_PROJECT1/Core/Inc/MQ2.h
@@ -0,0 +1,17 @@
+/*
+ * MQ2.h
+ *
+ *  Created on: Jan 1, 2026
+ *      Author: SUNBEAM
+ */
+
+#ifndef INC_MQ2_H_
+#define INC_MQ2_H_
+
+
+#include <stdint.h>
+
+uint32_t mq2_value(uint32_t adc_raw);
+
+
+#endif /* INC_MQ2_H_ */
